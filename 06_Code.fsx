bc85d240095d3e5629a6bd17780edad1567eb39e
Roman Provazník <dzoukr@dzoukr.cz>
2023-12-06 13:14:58 +0100

    Day 6

---
+++ 06_Code.fsx
@@ -0,0 +1,54 @@
+﻿#load "InputReader.fsx"
+
+open System
+
+let input = InputReader.read "06"
+
+let txt = """
+Time:      7  15   30
+Distance:  9  40  200""".Split("\n", StringSplitOptions.RemoveEmptyEntries ||| StringSplitOptions.TrimEntries)
+
+type Race = {
+    Time : int64
+    Distance : int64
+}
+
+let parseRaces (xs:string []) =
+    xs
+    |> Array.map (_.Split(":").[1].Split(" ", StringSplitOptions.RemoveEmptyEntries ||| StringSplitOptions.TrimEntries))
+    |> (fun x ->
+        let fstRow = x.[0]
+        [0..fstRow.Length - 1]
+        |> List.map (fun y ->
+            { Time = int x.[0].[y]; Distance = int x.[1].[y] }
+        )
+    )
+    
+let races = input |> parseRaces
+
+let asOne (rx:Race list) =
+    rx
+    |> List.fold (fun acc (item:Race) ->
+        (fst acc + string item.Time), (snd acc + string item.Distance) 
+    ) ("","")
+    |> (fun (t,d) -> { Time = int64 t; Distance = int64 d })
+    
+let calculateWins (r:Race) =
+    let tryTime (waitTime:int64) =
+        let time = r.Time - waitTime
+        let dist = time * waitTime
+        if dist > r.Distance then Some (waitTime) else None 
+    
+    [0L..r.Time]
+    |> List.choose tryTime
+
+let part1 =
+    races
+    |> List.map (calculateWins >> _.Length)
+    |> List.fold (*) 1
+
+let part2 =
+    races
+    |> asOne
+    |> calculateWins
+    |> List.length
